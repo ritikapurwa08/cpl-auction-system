9b7dd5d6b117f41fb6ac80640667072f131513e1
ritik-apurwa08 <8ballpoolrk2@gmail.com>
2025-03-08 21:03:23 +0530

    save them

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@convex": path.resolve(__dirname, "./convex"),
+    },
+  },
   server: {
     host: true,
   },
